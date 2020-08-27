0c471206d2a282be5ecded20437b4708241d60cb
ayoubix10 <68971473+ayoubix10@users.noreply.github.com>
2020-08-27 15:41:27 -0400

    First Commit

---
+++ src/DemoArray.java
@@ -0,0 +1,25 @@
+
+public class DemoArray {
+
+	public static void main(String[] args) {
+
+	int x[]= {10,12,8,34,67,98,90};
+	
+	//array has a length
+	//array values have indexes -- start with 0
+	//max index of an array is length - 1
+	
+	System.out.println(x.length);
+	System.out.println(x[0]);	
+	System.out.println(x[x.length-1]);	
+	
+    String names[] = {"Iqbal","Aqib","Sajeda","Moe"};
+	
+    System.out.println(names[0]);
+    System.out.println(names[names.length-1]);
+	
+	}
+
+	
+	
+}
---
+++ src/DisplayOddEven.java
@@ -0,0 +1,22 @@
+
+public class DisplayOddEven {
+
+
+	public static void main(String[] args) {
+
+		
+	int [] num = {3,2,100,87,45,33,99,10};
+	
+	for (int i = 0; i<num.length; i++) {
+		
+	if (num[i]%2==0)
+		
+	System.out.println(num [i] + " is an even number");	
+	else
+	System.out.println(num [i] + " is an odd number");	
+	}
+		
+	}
+
+}	
+
